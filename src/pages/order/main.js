b62b2b8008b78cf09202919c943d735293137f46
kaolalala <1921346727@qq.com>
2019-12-03 23:00:37 +0800

    没啥

--- src/pages/order/main.js
+++ src/pages/order/main.js
@@ -7,6 +7,9 @@ app.$mount()
 export default {
     config: {
         // 注意，页面级可配置属性相当于只是`src/main.js`中配置里的`window`部分
-        "navigationBarTitleText": "我的订单"  
+        "window": {
+            "navigationBarTitleText": "我的订单",
+            "navigationBarTitleText": "选择预约"
+        }
     }
 }
